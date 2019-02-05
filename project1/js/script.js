ae40cf76876bb31c820ce8df35ddc0f0dade979d
Percy Dragon <percy.s.dragon@gmail.com>
2019-02-05 17:18:08 -0500

    p1: added images, icons, made stuff draggable
    there's a lot going on here and I haven't commented or pushed enough, so let me give you the run down of what exactly I remember doing.
    
    So I refined the draggable images, I added in folder icons, I added in a dialogue box, I fixed the random positioning of the images, and I've added a background and custom font.

--- project1/js/script.js
+++ project1/js/script.js
@@ -20,25 +20,64 @@ you try to clean out the computer by putting everything in the trash, but only f
    $bin = $('#bin');
    $drag = $('.draggy');
 
-   $drag.draggable();
+   $drag.draggable({scroll:false});
 
    $bin.droppable( {
     drop: onDrop
   });
+  changePosition();
+  deleteRecycleBin();
+
+  // for (let i = 1; i < 100; i++) {
+  //   let $image = $('<img class="dragy" src="assets/images/' + i + '.png">');
+  //   $('body').append($image);
+  // }
+  //
 
  }
 
  function onDrop(event, ui) {
+   //making the draggable images hide when dropped into trash
    ui.draggable.hide();
    ui.draggable.removeClass('small');
+   //changed the image of trash if stuff is in it
+   $bin.attr('src','assets/images/hatsune_miku_full_recycle_bin.png');
 
    if ($('.small').length === 0) {
-     addClass();
-     $('.draggy').show();
+     $('#dialog-confirm').dialog('open');
+
    }
-   $bin.attr('src','assets/images/hatsune_miku_full_recycle_bin.png');
  }
 
- function addClass() {
+ function addClassSmall() {
+   //re-adds the small class to the images
    $drag.addClass('small');
+ }
+
+//setting random position to the images
+ function changePosition() {
+   $('.small').each(randomPosition);
+ }
+
+ function randomPosition(){
+   $(this).offset({top:Math.random()*600, left:Math.random()*1200});
+ }
+
+ function deleteRecycleBin() {
+   $("#dialog-confirm" ).dialog({
+     autoOpen: false,
+     resizable: false,
+     height: "auto",
+     width: 400,
+     modal: true,
+     buttons: {
+       "Delete all items": function() {
+         $( this ).dialog( "close" );
+         addClassSmall();
+         $('.draggy').show();
+         $bin.attr('src','assets/images/hatsune_miku_empty_recycle_bin.png');
+         changePosition();
+       }
+     }
+   });
  }
