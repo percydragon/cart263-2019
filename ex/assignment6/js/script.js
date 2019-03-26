e96cab506ebd277815e9dc0f4fdaebc20bcc7075
Percy Dragon <percy.s.dragon@gmail.com>
2019-03-26 10:54:33 -0400

    ex6: moved adjs to data, so it was all in one file, began working on additional word to string + article variable

--- ex/assignment6/js/script.js
+++ ex/assignment6/js/script.js
@@ -17,7 +17,6 @@ function setup() {
 }
 
 function dataLoaded(data) {
-  console.log(data);
   let condiment = getRandomElement(data.condiments);
   let verb = "is";
   if (condiment.charAt(condiment.length - 1) === "s") {
@@ -29,6 +28,21 @@ function dataLoaded(data) {
   console.log(verb, condiment);
   console.log(cat);
   console.log(room);
+
+    //adding in extra descriptive element
+
+    let adj = getRandomElement(data.adjs);
+    console.log(adj);
+
+//declaring article
+let article = "a";
+//adding in an if statement that dictates if the first letter is a vowel, it will be an 'an' instead of an 'a'
+
+
+// added adjs to the description
+  let description =  `${condiment} ${verb} like a ${adj} ${cat} in a ${room}`;
+  console.log(description);
+  $('body').append(description)
 }
 
 function getRandomElement(array) {
