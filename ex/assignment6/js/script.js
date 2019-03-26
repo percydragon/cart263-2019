5cc967e61f9455d311488aec85e037c709c46cce
Percy Dragon <percy.s.dragon@gmail.com>
2019-03-26 09:42:19 -0400

    ex6: added functions , arrays and selectors
    so I fetched the JSON data, and made a dataloaded function to load in the data from the JSON, but to select one element from the JSON, I made a getrandomelement function to select from one of the three arrays and return an element at random.
    
    I also added a verb that would detect whether the condiment word ended in s or not, so it can select between the verb is or are.

--- ex/assignment6/js/script.js
+++ ex/assignment6/js/script.js
@@ -9,3 +9,29 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+
+$(document).ready(setup);
+
+function setup() {
+  $.getJSON("assets/data/data.json", dataLoaded);
+}
+
+function dataLoaded(data) {
+  console.log(data);
+  let condiment = getRandomElement(data.condiments);
+  let verb = "is";
+  if (condiment.charAt(condiment.length - 1) === "s") {
+    verb = "are";
+  }
+
+  let cat = getRandomElement(data.cats);
+  let room = getRandomElement(data.rooms);
+  console.log(verb, condiment);
+  console.log(cat);
+  console.log(room);
+}
+
+function getRandomElement(array) {
+  let element = array[Math.floor(Math.random() * array.length)];
+  return element;
+}
