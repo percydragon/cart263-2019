fcc03263e977926a6352e860b827064064e5089f
Percy Dragon <percy.s.dragon@gmail.com>
2019-03-29 16:15:38 -0400

    ex7: added frequency array, new pizzicato sounds, playnote function and mouse pressed function

--- ex/assignment7/js/script.js
+++ ex/assignment7/js/script.js
@@ -2,13 +2,29 @@
 
 /*****************
 
-Title of Project
-Author Name
+Music Box
+Percy Dragon
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+
+let frequencies = [
+  220.00,
+  246.94,
+  277.18,
+  293.66,
+  329.63,
+  369.99,
+  415.30
+]
+
+let synth;
+let kick;
+let snare;
+let hithat;
+
 
 // preload()
 //
@@ -24,6 +40,34 @@ function preload() {
 // Description of setup
 
 function setup() {
+  synth = new Pizzicato.Sound({
+    source: 'wave',
+    options: {
+      type: 'sine',
+      frequency: 220
+    }
+  });
+
+  kick = new Pizzicato.Sound({
+    source: 'file',
+    options: {
+      path: 'assets/sounds/kick.wav'
+    }
+  });
+
+  snare = new Pizzicato.Sound({
+    source: 'file',
+    options: {
+      path: 'assets/sounds/snare.wav'
+    }
+  });
+
+  hihat = new Pizzicato.Sound({
+    source: 'file',
+    options: {
+      path: 'assets/sounds/hihat.wav'
+    }
+  });
 
 }
 
@@ -34,4 +78,15 @@ function setup() {
 
 function draw() {
 
+}
+
+function playNote() {
+  let frequency = frequencies[Math.floor(Math.random() * frequencies.length)];
+  synth.frequency = frequency;
+  synth.play();
+}
+
+function mousePressed() {
+  // Start an interval for the notes
+  setInterval(playNote,500);
 }
